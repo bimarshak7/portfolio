166087d07e2edccb55b1d3600cc1c650cc385820
bimarshak7 <pas076bct010@wrc.edu.np>
2020-07-29 19:50:50 +0545

    Updates

---
+++ precache-manifest.d9fd71324015aaba88a2e23432805aa1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67eb1bd9af9508e533ee17b1cb407943",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11ce6a3f1a2bb0022224",
+    "url": "/static/css/main.07479b01.chunk.css"
+  },
+  {
+    "revision": "d5412068aa7995a606cd",
+    "url": "/static/js/2.0d667ae5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0d667ae5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11ce6a3f1a2bb0022224",
+    "url": "/static/js/main.a6e77047.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "3c9d56d121c58f2235c191c034b1ff99",
+    "url": "/static/media/dark.3c9d56d1.jpeg"
+  },
+  {
+    "revision": "9609e48b259d75baa5e680e308f1d00d",
+    "url": "/static/media/shiva.9609e48b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e58ace1ae1ea05019ee2f1b6b12401af.js"
+  "/precache-manifest.d9fd71324015aaba88a2e23432805aa1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
